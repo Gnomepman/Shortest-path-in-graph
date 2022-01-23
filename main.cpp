06786c296a74c9d1f49acaf591bc6048d659192d
Danya <danilomaster2003@ukr.net>
2022-01-23 18:41:15 +0200

    Translated GUI

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("Лабораторна робота №6, Деркач Д.А. ПІ-201");
+    w.setWindowTitle("Shortest path");
     w.show();
     return a.exec();
 }
